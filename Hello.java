779f3b14a1813d05d6a38e393cb7f2710766594a
SAEP68 <91881011+SAEP68@users.noreply.github.com>
2022-09-14 15:49:12 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Hello {
-    public static void main(String[] args) {
-       // Your code goes here the lines below will help
-       // but are not an answer to the tutorial
-       Scanner scanner = new Scanner(System.in);
+        String line;
+        Scanner scanner = new Scanner(System.in);
 
-       System.out.println("Your text");
-       System.out.println("Your answer");
+        System.out.println("What is your name?");
+        line = scanner.nextLine();
+        System.out.println("Hello " + line);
     }
 }
